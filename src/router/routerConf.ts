5d7f83113ab30b75e39375d4689614aa17883e1f
nankanull <534129087@qq.com>
2019-11-20 11:33:26 +0800

    剔除無關組件

--- src/router/routerConf.ts
+++ src/router/routerConf.ts
@@ -1,48 +1,14 @@
 import Home from '@/pages/home';
-import BookRank1 from '@/pages/common/bookrank';
-import BookRank2 from '@/pages/common/bookRank1';
-import BookRank3 from '@/pages/common/bookrank2';
-import List from '@/pages/list';
-import Login from '@/pages/login';
 import MainLayout from '@/layout/mainLayout';
-import CommonLayout from '@/layout/commonLayout';
 import Exception from '@/pages/exception';
 
 export const routerConf = [
   {
     path: '/',
     redirect: '/home',
-  },
-  {
+  }, {
     path: '/home',
     component: Home,
-    layout: MainLayout
-  },
-  {
-    path: '/common',
-    redirect: '/common/bookRank1',
-    children: [{
-      path: '/bookRank1',
-      component: BookRank1,
-      layout: CommonLayout
-    },{
-      path: '/bookRank2',
-      component: BookRank2,
-      layout: CommonLayout
-    },{
-      path: '/bookRank3',
-      component: BookRank3,
-      layout: CommonLayout
-    }]
-  },
-  {
-    path: '/list',
-    component: List,
-    layout: MainLayout
-  },
-  {
-    path: '/login',
-    component: Login,
     layout: MainLayout
   }, {
     path: '*',
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,7 +1,5 @@
-import { bookReducer } from '@/layout/commonLayout/models';
 import { globalReducer } from './globalReducer';
 
 export const appReducer = {
-  book: bookReducer,
   global: globalReducer
 };
