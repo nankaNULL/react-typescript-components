f30b6377f1d18708f665591ab1399db1d92c9d12
nankanull <534129087@qq.com>
2019-11-25 16:43:42 +0800

    tag组件

---
+++ src/components/tag/index.tsx
@@ -0,0 +1,72 @@
+import React, { Fragment } from 'react';
+import { Tag } from 'antd';
+import classNames from 'classnames';
+import MyIcon from '@/components/icon';
+import './style.scss';
+import MyButton from '../button';
+
+interface TagProps {
+  children?: React.ReactNode;
+  closable?: Boolean;
+  onClose?: Function;
+  visible?: Boolean;
+  color?: string;
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+interface TagState {
+  visible: Boolean;
+}
+
+export default class MyTag extends React.PureComponent<TagProps, TagState> {
+  constructor(props: TagProps) {
+    super(props);
+    this.state = {
+      visible: ('visible' in props ? props.visible : true) as Boolean
+    }
+  }
+
+  // 点击关闭图标
+  handleIconClose = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    const { closable, onClose } = this.props;
+    if (closable && onClose) {
+      onClose(e);
+    }
+    if (!('visible' in this.props)) {
+      this.setState({ visible: false })
+    }
+  }
+
+  // tag - class
+  getTagClassName = () => {
+    const { color, className } = this.props;
+    const { visible } = this.state;
+    return classNames('tag', {
+      [`tag-hidden`]: !visible,
+      [`tag-has-color`]: Boolean(color),
+    }, className)
+  }
+
+  // tag 样式
+  getTagStyle = () => {
+    const { color } = this.props;
+    return {
+      backgroundColor: color,
+      borderColor: color
+    }
+  }
+
+  render() {
+    const { children, closable } = this.props;
+    return <Fragment>
+      <div>myTag</div>
+      <div>
+        <span className={this.getTagClassName()} style={this.getTagStyle()}>
+          {children}
+          {closable && <MyIcon type="close" className="tag-close" onClick={this.handleIconClose} />}
+        </span>
+      </div>
+    </Fragment>
+  }
+}
